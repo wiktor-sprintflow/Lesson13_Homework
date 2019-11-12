6f49ef2741307e70c6bc19eb5e9dd3cfda2d8441
Wiktor Kapanowski <wiktor.kapanowski@gmail.com>
2019-11-12 01:07:51 +0100

    Lesson 13, Homework - task 2

---
+++ src/task02/MathApp.java
@@ -0,0 +1,54 @@
+package task02;
+
+import java.io.*;
+
+public class MathApp {
+    private static final String INPUT_FILE = "math_input.txt";
+    private static final String OUTPUT_FILE = "math_output.txt";
+
+    public static void main(String[] args) {
+        File inputFile = new File(INPUT_FILE);
+        File outputFile = new File(OUTPUT_FILE);
+
+        try (FileReader fileReader = new FileReader(inputFile);
+             BufferedReader bufferedReader = new BufferedReader(fileReader);
+             FileWriter fileWriter = new FileWriter(outputFile);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)
+        ) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line + " = " + calculateResult(line));
+                bufferedWriter.write(line + " = " + calculateResult(line));
+                bufferedWriter.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static double calculateResult(String line) {
+        String[] mathOperation = line.split(" ");
+        String operator = mathOperation[1];
+
+        double result;
+        switch (operator) {
+            case "+":
+                result = (Double.parseDouble(mathOperation[0]) + Double.parseDouble(mathOperation[2]));
+                break;
+            case "-":
+                result = (Double.parseDouble(mathOperation[0]) - Double.parseDouble(mathOperation[2]));
+                break;
+            case "*":
+                result = (Double.parseDouble(mathOperation[0]) * Double.parseDouble(mathOperation[2]));
+                break;
+            case "/":
+                result = (Double.parseDouble(mathOperation[0]) / Double.parseDouble(mathOperation[2]));
+                break;
+            default:
+                result = 0;
+        }
+        return result;
+    }
+}
